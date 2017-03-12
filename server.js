72960d61e89d4c859d438547e4a824a2b9b90c7d
WRyba <wojryba@gmail.com>
2017-03-12 17:40:06 +0100

    check

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var UAParser = require('ua-parser-js');
 //https://github.com/florrain/locale
 var locale = require("locale");
 
+var ipaddr = require('ipaddr.js');
 
 var app = express();
 
@@ -15,8 +16,11 @@ app.get('/', function(req, res){
   var ua = req.headers['user-agent'];
   var c = parser.setUA(ua).getOS();
   var l = locale.Locale["default"];
+  var ip = req.ip;
+  ip = ipaddr.process(ip);
+  ip = ip.toString();
   var resObj = {
-    "ipaddress": req.ip,
+    "ipaddress": ip,
     "language": l,
     "software": c
   };
