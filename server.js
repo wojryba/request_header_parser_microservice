0e6aca5ff72abe154165c6b107071d4d95ad4aba
WRyba <wojryba@gmail.com>
2017-03-12 17:48:59 +0100

    finished

--- server.js
+++ server.js
@@ -6,23 +6,31 @@ var locale = require("locale");
 
 var ipaddr = require('ipaddr.js');
 
-var app = express();
 
+var app = express();
+app.enable('trust proxy');
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
 
+  //get the OS
   var parser = new UAParser();
   var ua = req.headers['user-agent'];
   var c = parser.setUA(ua).getOS();
+
+  //get Language
   var l = locale.Locale["default"];
+
+  //get ip
   var ip = req.ip;
   ip = ipaddr.process(ip);
   ip = ip.toString();
+
+  //formulate an answer and sent it
   var resObj = {
     "ipaddress": ip,
     "language": l,
-    "software": c
+    "software": c.name +" "+ c.version
   };
   res.send(resObj);
 })
